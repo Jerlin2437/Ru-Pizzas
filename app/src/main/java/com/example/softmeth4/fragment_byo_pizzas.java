ea2162716eae88d11409c4884cb99f0e4e1f51ae
leijason22 <leijason22@gmail.com>
2023-12-06 12:38:26 -0500

    Update fragment_byo_pizzas.java

--- app/src/main/java/com/example/softmeth4/fragment_byo_pizzas.java
+++ app/src/main/java/com/example/softmeth4/fragment_byo_pizzas.java
@@ -7,58 +7,66 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import com.example.softmeth4.businesslogic.Order;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link fragment_byo_pizzas#newInstance} factory method to
+ * Use the factory method to
  * create an instance of this fragment.
  */
 public class fragment_byo_pizzas extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+  private Order order;
+  private Spinner spinner;
+  private ListView toppingsListView;
+  private ArrayAdapter<String> spinnerAdapter;
+  private ArrayAdapter<String> toppingsAdapter;
+  private List<String> sizeList = Arrays.asList("Small", "Medium", "Large");
+  private List<String> toppingsList = Arrays.asList("Sausage", "Beef", "Pepperoni", "Ham", "Onion", "Green Pepper",
+          "Mushroom", "Black Olive", "Shrimp", "Squid", "Crab Meat", "Spam", "Fish");
 
     public fragment_byo_pizzas() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_byo_pizzas.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static fragment_byo_pizzas newInstance(String param1, String param2) {
-        fragment_byo_pizzas fragment = new fragment_byo_pizzas();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+//    public static fragment_byo_pizzas newInstance(String param1, String param2) {
+//        fragment_byo_pizzas fragment = new fragment_byo_pizzas();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        order = Order.getInstance();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_byo_pizzas, container, false);
+        View view = inflater.inflate(R.layout.fragment_byo_pizzas, container, false);
+        spinner = view.findViewById(R.id.buildSpinner);
+        // Initialize ArrayAdapter
+        spinnerAdapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, sizeList);
+        // Set the dropdown layout style
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Set the adapter to the Spinner
+        spinner.setAdapter(spinnerAdapter);
+
+        toppingsListView = view.findViewById(R.id.notSelectedToppings);
+        toppingsAdapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, toppingsList);
+        toppingsListView.setAdapter(toppingsAdapter);
+
+        return view;
     }
 }
